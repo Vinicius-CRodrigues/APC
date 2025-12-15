b1050e881b005e9360a5cd9a22ca77353a372633
Vinicius Cardoso Rodrigues <89284661+Vinicius-CRodrigues@users.noreply.github.com>
2025-12-15 03:31:57 -0300

    Delete Revisão aula directory

--- Revisão aula/q2.py
+++
@@ -1,9 +0,0 @@
-digito, inicio, fim = input().split()
-
-inicio, fim = int(inicio), int(fim)
-
-list_count_8 = [str(n).count(str(digito)) for n in range (inicio, fim + 1)]
-
-total = sum(list_count_8)
-
-print(total)
--- Revisão aula/q4.py
+++
@@ -1,8 +0,0 @@
-def prange(start, stop, step):
-    if start >= stop:
-        return []
-    
-    return [start] + prange(start + step, stop, step)
-
-
-print(prange(1, 9, 2))
