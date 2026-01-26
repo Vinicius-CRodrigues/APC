4066c0c044df09133771233af2baad914de006ec
Vinicius Cardoso <vinicius.cardoso.vcr@gmail.com>
2026-01-26 19:04:23 -0300

    Adição de novos exercícios

---
+++ Praticando python/Lista 2/q4.py
@@ -0,0 +1,12 @@
+'''
+Armano trabalha com a gestão de dois estoques de mercadorias que são representados como tuplas. Agora, ele precisa criar um relatório unificado com os produtos dos dois estoques juntos.
+
+Para ajudá-lo, como você criaria um programa que ler as informações dos estoques e gera um relatório com todos os produtos juntos?
+'''
+
+produtos = input('Produtos do estoque 1 (separados por vírgula): ').split(',')
+
+produtos_2 = input('Produtos do estoque 2 (separados por vírgula): ').split(',')
+
+print(produtos + produtos_2)
+
---
+++ Praticando python/Lista 2/q5.py
@@ -0,0 +1,19 @@
+'''
+Camila adora receber amigos para jantares temáticos. Para o próximo encontro, ela quer garantir que a ordem de chegada seja respeitada, mas ainda precisa fazer ajustes na lista de convidados. Camila quer adicionar novos nomes e organizá-los em posições específicas.
+
+Como você criaria um programa que mostre a lista inicial, permita a inserção de um novo nome em uma posição escolhida e exiba a lista atualizada?
+'''
+
+lista_convidados = ['João', 'Maria', 'Pedro', 'Ana']
+
+print('Lista atual de convidados: ', lista_convidados)
+
+
+novo_convidado = input('Digite o nome do novo convidado: ')
+
+posicao = int(input('Digite a posição na qual deseja inserir o convidado: '))
+
+lista_convidados[posicao - 1] = novo_convidado
+
+print('Lista atualizada de convidados: ', lista_convidados)
+
